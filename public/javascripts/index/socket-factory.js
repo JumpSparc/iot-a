86ced7dfcb606f5efb1e98e6c913c2f0366e54b9
Rick <rickizta@gmail.com>
2015-03-23 13:12:30 +0800

    /push changed to get

--- public/javascripts/index/socket-factory.js
+++ public/javascripts/index/socket-factory.js
@@ -1,5 +1,5 @@
 angular.module('SolarProject')
 .factory('socket', function SocketFactory() {
-	var socket = io.connect('http://localhost:3000/');
+	var socket = io.connect('http://solar-test.herokuapp.com/');
 	return socket;
 });

af460423b9bb476b8b53b89d11732dfe9a4042e1
Rick <rickizta@gmail.com>
2015-04-12 04:21:19 +0800

    updated chart, now using d3

--- public/javascripts/index/socket-factory.js
+++ public/javascripts/index/socket-factory.js
@@ -1,6 +1,6 @@
 angular.module('SolarProject')
 .factory('socket', function SocketFactory() {
-	var socket = io.connect('http://localhost:3000');
+	var socket = io.connect('http://solar-test.herokuapp.com/');
 	return socket;
 });
 
